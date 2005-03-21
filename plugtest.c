ec16b26b28ff6a2f028ca4039d2a84844e471a6d
Steve Slaven <bpk@hoopajoo.net>
2005-03-21 21:35:42 +0000

    use tty_printf since printf will not work with locales

--- plugtest.c
+++ plugtest.c
@@ -8,11 +8,11 @@ void plugtest( char *arg );
 cmdstruct mycommand = { NULL, "plugtest", "test command", plugtest, NULL };
 
 void powwow_init() {
-	printf( "Init plugtest.so!\n" );
+	tty_printf( "Init plugtest.so!\n" );
 
 	cmd_add_command( &mycommand );
 }
 
 void plugtest( char *arg ) {
-	printf( "Arg was '%s'\n", arg );
+	tty_printf( "Arg was '%s'\n", arg );
 }
