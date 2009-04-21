01a9f65baec90120a33a69128deb215a419e1a85
Steve Slaven <bpk@hoopajoo.net>
2009-04-21 11:31:33 -0700

    Update license to gpl2+

--- plugtest.c
+++ plugtest.c
@@ -4,6 +4,16 @@
 #include "cmd.h"
 #include "tty.h"
 
+/* Bare test plugin for powwow
+ * Author: Steve Slaven - http://hoopajoo.net
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ */
+
 void plugtest( char *arg );
 
 cmdstruct mycommand = { NULL, NULL, "plugtest", plugtest, "test command" };
--- powwow-muc.c
+++ powwow-muc.c
@@ -5,6 +5,12 @@
 
 /* Curses based powwow movie player.
  * Author: Steve Slaven - http://hoopajoo.net
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
  */
 
 #define JUMP_SMALL     1000
