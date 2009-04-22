77db0e38144cb8421c9a742bccfca415b239495e
Steve Slaven <bpk@hoopajoo.net>
2009-04-22 08:12:40 -0700

    Changed to gpl2+

--- catrw.c
+++ catrw.c
@@ -1,7 +1,11 @@
 /*
  *  catrw.c  --  open a file with O_RDWR and print it.
  * 
- *  This file is placed in the public domain.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
--- follow.c
+++ follow.c
@@ -1,7 +1,11 @@
 /*
  *  follow.c  --  interactively print an ASCII file.
  * 
- *  This file is placed in the public domain.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
  */
 
 #include <stdio.h>
